3c8d70720641f68dea12315f5478eaeff68834a2
nicolas <nicolas@damiens.info>
2012-02-28 08:26:16 +0100

    init

---
+++ gen/R.java.d
@@ -0,0 +1,4 @@
+/home/nicolas/android-sdk-linux/proj4/gen/org/osgeo/proj4j/R.java \
+ : /home/nicolas/android-sdk-linux/proj4/res/layout/main.xml \
+/home/nicolas/android-sdk-linux/proj4/res/values/strings.xml \
+/home/nicolas/android-sdk-linux/proj4/AndroidManifest.xml \
